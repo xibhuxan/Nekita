ab5f135b2566d7eac8fd0c26d50c438f592bec9b
Xibhuxan <xibhuxan@gmail.com>
2023-11-16 00:36:40 +0000

    Update

--- src/nekomando.py
+++ src/nekomando.py
@@ -10,7 +10,14 @@ class Nekomandos:
         self.chat_id = mensaje.chat_id
         self.usuario = mensaje.from_user
         self.contenido = mensaje.text
-       
+        self.nombre = self.usuario.username
+        
+        if self.nombre is None:
+            self.nombre = self.usuario.first_name
+        else:
+            self.nombre = '@' + self.nombre
+
+
         self.comandos_respuestas = [
         ComandoRespuesta("hola china", hora.hora_zona_horaria("Asia/Shanghai")),
         ComandoRespuesta("hora", hora.hora_zona_horaria("Europe/Madrid")),
@@ -20,17 +27,17 @@ class Nekomandos:
         ComandoRespuesta("pole canaria", "A las 12:00 de canarias"),
         ComandoRespuesta("pole peninsular", "A las 11:00 de canarias"),
         ComandoRespuesta("pole andaluza", "Hazla cuando quieras, pero a las 06:00 península está bien-nya."),
-        ComandoRespuesta("pole patitos", "22:22 Tú puedes-nya!"),
-        ComandoRespuesta("pole cuack", "22:22 Tú puedes-nya!"),
-        ComandoRespuesta("pole patitos canarias", "22:22 Tú puedes-nya!"),
-        ComandoRespuesta("pole cuack canarias", "22:22 Tú puedes-nya!"),
+        ComandoRespuesta("pole patitos", "A las 22:22, tú puedes-nya!"),
+        ComandoRespuesta("pole cuack", "A las 22:22, tú puedes-nya!"),
+        ComandoRespuesta("pole patitos canarias", "A las 22:22, tú puedes-nya!"),
+        ComandoRespuesta("pole cuack canarias", "A las 22:22, tú puedes-nya!"),
         ComandoRespuesta("dobby", f"Dobby no tiene amo Dobby es un elfo libre"),
         ComandoRespuesta(":)", ":)"),
         ComandoRespuesta(":(", "Sonríe, eres especial-nya!!"),
         ComandoRespuesta(":3", "¯\_(=^･ω･^)_/¯"),
-        ComandoRespuesta("buenos dias", f"Buenos días @{self.usuario.username} para ti también."),
-        ComandoRespuesta("buenas tardes", f"Buenas tardes @{self.usuario.username} ¿Ya has comido-nya?"),
-        ComandoRespuesta("buenas noches", f"Buenas noches @{self.usuario.username} que duermas bien-NYA!!"),
-        ComandoRespuesta("buenas", f"Hola @{self.usuario.username} ¿Qué tal?."),
-        ComandoRespuesta("hola", f"Hola @{self.usuario.username} ¿Qué tal?."),
+        ComandoRespuesta("buenos dias", f"Buenos días @{self.nombre} para ti también."),
+        ComandoRespuesta("buenas tardes", f"Buenas tardes {self.nombre}, ¿ya has comido-nya?"),
+        ComandoRespuesta("buenas noches", f"Buenas noches {self.nombre}, que duermas bien-NYA!!"),
+        ComandoRespuesta("buenas", f"Hola, {self.nombre}, ¿qué tal?"),
+        ComandoRespuesta("hola", f"Hola, {self.nombre}, ¿qué tal?"),
     ]
